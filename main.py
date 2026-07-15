63246b71f832ab2dd1e4a99e999ef5c62b120e57
xiayuku63 <417451773@qq.com>
2026-07-15 18:18:01 +0800

    style: format main entrypoint

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == "__main__":
     reload_ = os.getenv("UVICORN_RELOAD", "true").lower() in {"1", "true", "yes", "on"}
 
     import uvicorn.config
+
     uvicorn.run("main:app", host=host, port=port, reload=reload_)
